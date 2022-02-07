d62aad32b6c31747a4f7e1ca7db81c855f8a5185
mostaphaRoudsari <sadeghipour@gmail.com>
2022-02-07 18:00:44 -0500

    feat: add daylight and view matrix calculation
    Also added matrix multiplication

--- pollination/three_phase/entry.py
+++ pollination/three_phase/entry.py
@@ -3,7 +3,11 @@ from dataclasses import dataclass
 from pollination.honeybee_radiance.translate import CreateRadianceFolderGrid
 from pollination.honeybee_radiance.octree import CreateOctree
 from pollination.honeybee_radiance.sky import CreateSkyDome, CreateSkyMatrix
+from pollination.honeybee_radiance.multiphase import DMTXGroup
 
+from ._view_matrix import ViewMatrixRayTracing
+from ._daylight_matrix import DaylightMtxRayTracing
+from ._multiply_matrix import MultiplyMatrixDag
 
 @dataclass
 class ThreePhaseEntryPoint(DAG):
@@ -35,7 +39,7 @@ class ThreePhaseEntryPoint(DAG):
 
     radiance_parameters = Inputs.str(
         description='The radiance parameters for ray tracing.',
-        default='-ab 2 -ad 5000 -lw 2e-05'
+        default='-ab 1 -ad 100 -lw 0.01'
     )
 
     grid_filter = Inputs.str(
@@ -57,7 +61,7 @@ class ThreePhaseEntryPoint(DAG):
     )
 
     @task(template=CreateRadianceFolderGrid)
-    def create_rad_folder(self, input_model=model, grid_filter=grid_filter):
+    def _create_rad_folder(self, input_model=model, grid_filter=grid_filter):
         """Translate the input model to a radiance folder."""
         return [
             {
@@ -69,18 +73,18 @@ class ThreePhaseEntryPoint(DAG):
                 'to': 'model/bsdf'
             },
             {
-                'from': CreateRadianceFolderGrid()._outputs.sensor_grids_file,
-                'to': 'results/total/grids_info.json'
-            },
-            {
                 'from': CreateRadianceFolderGrid()._outputs.sensor_grids,
                 'description': 'Sensor grids information.'
+            },
+            {
+                'from': CreateRadianceFolderGrid()._outputs.states,
+                'description': 'Dynamic blinds states information.'
             }
         ]
 
-    @task(template=CreateOctree, needs=[create_rad_folder])
+    @task(template=CreateOctree, needs=[_create_rad_folder])
     def create_octree(
-            self, model=create_rad_folder._outputs.model_folder,
+            self, model=_create_rad_folder._outputs.model_folder,
             include_aperture='exclude', black_out='default'
         ):
         """Create octree from radiance folder."""
@@ -91,40 +95,117 @@ class ThreePhaseEntryPoint(DAG):
             }
         ]
 
-    @task(template=CreateOctree, needs=[create_rad_folder])
-    def create_octree_direct(
-        self, model=create_rad_folder._outputs.model_folder,
-        include_aperture='exclude', black_out='black'
-        ):
-        """Create octree from radiance folder for direct studies."""
-        return [
-            {
-                'from': CreateOctree()._outputs.scene_file,
-                'to': 'octrees/direct.oct'
-            }
-        ]
-
     @task(template=CreateSkyDome)
     def create_sky_dome(self):
         """Create sky dome for daylight coefficient studies."""
         return [
-            {'from': CreateSkyDome()._outputs.sky_dome, 'to': 'assets/sky_dome.rad'}
+            {
+                'from': CreateSkyDome()._outputs.sky_dome,
+                'to': 'sky_vectors/sky_dome.rad'
+            }
         ]
 
     @task(template=CreateSkyMatrix)
     def create_total_sky(self, north=north, wea=wea, sun_up_hours='sun-up-hours'):
         return [
-            {'from': CreateSkyMatrix()._outputs.sky_matrix,
-             'to': 'sky_vectors/sky.mtx'}
+            {
+                'from': CreateSkyMatrix()._outputs.sky_matrix,
+                'to': 'sky_vectors/sky.mtx'
+            }
         ]
 
-    @task(template=CreateSkyMatrix)
-    def create_direct_sky(
-        self, north=north, wea=wea, sky_type='sun-only', sun_up_hours='sun-up-hours'
+
+    @task(
+        template=ViewMatrixRayTracing,
+        needs=[_create_rad_folder, create_octree],
+        loop=_create_rad_folder._outputs.sensor_grids,
+        sub_folder='initial_results/view_mtx',
+        sub_paths={
+            'sensor_grid': 'grid/{{item.full_id}}.pts',
+            'receiver_file': 'receivers/{{item.full_id}}..receiver.rad',
+            'receivers_folder': 'aperture_group'
+        }
+    )
+    def calculate_view_matrix(
+        self,
+        grid_name='{{item.full_id}}',
+        radiance_parameters=radiance_parameters,
+        sensor_count='{{item.count}}',
+        receiver_file=_create_rad_folder._outputs.model_folder,
+        sensor_grid=_create_rad_folder._outputs.model_folder,
+        scene_file=create_octree._outputs.scene_file,
+        receivers_folder=_create_rad_folder._outputs.model_folder,
+        bsdf_folder=_create_rad_folder._outputs.bsdf_folder
+    ):
+        pass
+
+    @task(template=DMTXGroup, needs=[_create_rad_folder, create_octree, create_sky_dome])
+    def group_apertures(
+        self,
+        model_folder=_create_rad_folder._outputs.model_folder,
+        scene_file=create_octree._outputs.scene_file,
+        sky_dome=create_sky_dome._outputs.sky_dome
     ):
         return [
             {
-                'from': CreateSkyMatrix()._outputs.sky_matrix,
-                'to': 'sky_vectors/sky_direct.mtx'
+                'from': DMTXGroup()._outputs.grouped_apertures_folder,
+                'to': 'initial_results/grouped_apertures'
+            },
+            {
+                'from': DMTXGroup()._outputs.matrix_mapping
+            },
+            {
+                'from': DMTXGroup()._outputs.matrix_mapping_file,
+                'to': 'results/info.json'
+            },
+            {
+                'from': DMTXGroup()._outputs.grouped_apertures
             }
         ]
+
+    @task(
+        template=DaylightMtxRayTracing,
+        sub_folder='initial_results/daylight_mtx',
+        loop=group_apertures._outputs.grouped_apertures,
+        needs=[_create_rad_folder, create_octree, create_sky_dome, group_apertures],
+        sub_paths={
+            'sender_file': '{{item}}.rad',
+            'senders_folder': 'aperture_group'
+        }
+    )
+    def daylight_mtx_calculation(
+        self,
+        name='{{item}}',
+        radiance_parameters=radiance_parameters,
+        receiver_file=create_sky_dome._outputs.sky_dome,
+        sender_file=group_apertures._outputs.grouped_apertures_folder,
+        senders_folder=_create_rad_folder._outputs.model_folder,
+        scene_file=create_octree._outputs.scene_file,
+        bsdf_folder=_create_rad_folder._outputs.bsdf_folder
+    ):
+        pass
+
+    # multiply all the matrices for all the states
+    @task(
+        template=MultiplyMatrixDag,
+        sub_folder='results',
+        loop=group_apertures._outputs.matrix_mapping,
+        needs=[group_apertures, daylight_mtx_calculation, create_total_sky],
+        sub_paths={
+            'view_matrix': '{{item.vmtx}}.vmtx',
+            't_matrix': '{{item.tmtx}}',
+            'daylight_matrix': '{{item.dmtx}}.dmtx'
+        }
+    )
+    def multiply_matrix(
+        self,
+        identifier='{{item.identifier}}',
+        sky_vector=create_total_sky._outputs.sky_matrix,
+        view_matrix='initial_results/view_mtx',
+        t_matrix='model/bsdf',
+        daylight_matrix='initial_results/daylight_mtx'
+    ):
+        pass
+
+
+results = Outputs.folder(source='results')
