cb5796e9ba7ba258b1c6fe05aef65ad69628cb33
mostaphaRoudsari <sadeghipour@gmail.com>
2022-02-14 18:41:11 -0500

    tests: fix test

--- tests/validation_test.py
+++ tests/validation_test.py
@@ -1,8 +1,8 @@
-from pollination.three_phase.entry import ThreePhaseEntryPoint
+from pollination.three_phase.entry import RecipeEntryPoint
 from queenbee.recipe.dag import DAG
 
 
 def test_three_phase():
-    recipe = ThreePhaseEntryPoint().queenbee
-    assert recipe.name == 'three-phase-entry-point'
+    recipe = RecipeEntryPoint().queenbee
+    assert recipe.name == 'recipe-entry-point'
     assert isinstance(recipe, DAG)
