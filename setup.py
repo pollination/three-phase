108265cf99dc62bef9ab1b5c96778bb75068f6a2
mostaphaRoudsari <sadeghipour@gmail.com>
2022-02-14 21:19:29 -0500

    build(ci): add GitHub Actions

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     setup_requires=['setuptools_scm'],
     url='https://github.com/pollination/three-phase',                   # will be translated to home
     project_urls={
-        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/annualrecipe.png',
+        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/3phaserecipe.png',
         'docker': 'https://hub.docker.com/r/ladybugtools/honeybee-radiance'
     },
     description='Three phase recipe for Pollination.',                  # will be used as package description
